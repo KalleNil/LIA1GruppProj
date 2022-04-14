4efb1f84cfcaccf93b5e218fc311cde9188930f3
Kalle <domare2@hotmail.com>
2022-04-15 00:43:23 +0200

    funktion för att lägga till nya annonser. Eget id och användarinmatning saknas.

---
+++ script.js
@@ -0,0 +1,7 @@
+function newAnnons(){
+    document.getElementById("annonser").innerHTML += 
+    "<a class='card' href='#!'><div id='kort1Front' class='front'><p>Klicka på knappen högst upp</p></div><div class='back'><div id='kort1Back'><p>Klicka på knappen högst upp</p></div>";
+}
+
+
+
