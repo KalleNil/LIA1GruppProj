5aed7793af0704af1195fc6f804feb732f2fb8e2
Kalle <domare2@hotmail.com>
2022-04-17 23:39:11 +0200

    kategori för produkterna samt nya produkter

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //produkterna/annonserna
 const annonser = [
     { id: 1, title: "Tavla",category: "Konst", description: "En väldigt unik tavla från Antwerp", price: 123, url:  "https://cdn.newport.se/gallery/4142/goc-fageltavla-laura-2__fullsize.jpg"},
-    { id: 2, title: "Ost",category: "Konst", description: "En välsmakande ost gjord på get", price: 456 },
-    { id: 3, title: "Skinka",category: "Konst", description: "Rökt skinka från Dalarna", price: 789 },
+    { id: 2, title: "Volvo XC40",category: "Fordon", description: "En robust och pålitlig bil", price: 450000, url: "https://images.ctfassets.net/x7m88j6y655h/4GtEYKDIZQDFBXQTXIX6Xi/2f914f12f1ed3b2cf1e657539ee2bf21/volvo.jpg"},
+    { id: 3, title: "Villa",category: "Hus", description: "Ett nybyggt hus i en modern stil", price: 4000000 , url: "https://www.trivselhus.se/imagevault/publishedmedia/p5nyji3581eajlmtwv1t/trivselhus_hemma_hos_villa_mariestad_0758.jpg"},
 ];
 //skapar en annons för alla produkterna
 for(let i = 0; i<annonser.length; i++){
     document.getElementById("annonser").innerHTML += 
-    "<a class='card' href='#!'><div id='kort"+annonser[i].id +"Front' class='front'><h3>"+annonser[i].title+"</h3><p>hej</p><br> <p class='olle'>"+annonser[i].price+"kr</p></div><div class='back'><div id='kort"+annonser[i].id+"Back'><p>"+annonser[i].description+"</p> </div> <button>KÖP</button>";
+    "<a class='card' href='#!'><div id='kort"+annonser[i].id +"Front' class='front'><h3>"+annonser[i].title+"</h3><p>"+annonser[i].category+"</p><br> <p class='olle'>"+annonser[i].price+"kr</p></div><div class='back'><div id='kort"+annonser[i].id+"Back'><p>"+annonser[i].description+"</p> </div> <button>KÖP</button>";
     document.getElementById('kort'+annonser[i].id+'Front').style.backgroundImage = "url("+annonser[i].url+")";
     
 }
